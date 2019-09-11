aeccb66a96a381e7ddc248e1ad539f9f80d3d588
LuisFOsG <luisfosgdev@gmail.com>
2019-09-11 17:37:06 -0500

    feat(Root): verify URL query

--- src/search.js
+++ src/search.js
@@ -3,9 +3,22 @@ import { $ } from './domElements'
 const form = $('#search')
 const input = $('#search-input')
 
+function urlify (text) {
+  const urlRegex = /(https?:\/\/[^\s]+)/g
+
+  if (text.trim().includes(' ')) return false
+  return urlRegex.test(text)
+}
+
 const searchQuery = (e) => {
   e.preventDefault()
-  window.location.href = `https://www.google.com/search?q=${input.value.trim()}`
+  const query = input.value.trim()
+  const isURL = urlify(query)
+
+  isURL
+    ? window.location.href = query
+    : window.location.href = `https://www.google.com/search?q=${query}`
+
   e.target.reset()
 }
 
