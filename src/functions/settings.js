a799ba84baedcf17ad982967a5edf448acda44aa
LuisFOsG <luisfosgdev@gmail.com>
2019-11-10 15:18:56 -0500

    feat(Root): close modal

--- src/functions/settings.js
+++ src/functions/settings.js
@@ -2,7 +2,14 @@ import { $, addEvent } from '@/util/domElements'
 
 const settings = $('#settings')
 
-addEvent($('#settings-btn'), 'click', function () {
-  settings.classList.add('visible')
-  settings.classList.remove('invisible')
+const changeVisibility = () => {
+  settings.classList.toggle('visible')
+  settings.classList.toggle('invisible')
+}
+
+addEvent($('#settings-btn'), 'click', changeVisibility)
+addEvent($('#close_modal'), 'click', changeVisibility)
+
+window.addEventListener('click', (e) => {
+  if (e.target === settings) changeVisibility()
 })
