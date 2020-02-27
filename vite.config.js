eda90dc5c27e8c867df8c2e1d2f50b0dbb5be9c4
LuisFOsG <luisfosgdev@gmail.com>
2020-02-27 16:35:00 -0500

    chore(Root): add conditional vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
 import { defineConfig } from 'vite'
-
 import { minifyHtml } from 'vite-plugin-html'
 
 import { resolve } from 'path'
 
-export default defineConfig({
-  plugins: [
-    minifyHtml()
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
+export default defineConfig(() => {
+  if (process.env.VITE_EXTENSION) {
+    return {
+      plugins: [
+        minifyHtml()
+      ],
+      resolve: {
+        alias: {
+          '@': resolve(__dirname, './src')
+        }
+      }
+    }
+  }
+
+  return {
+    plugins: [
+      minifyHtml()
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src')
+      }
     }
   }
 })
