f5ffe7fbaff152aafda45dca4b882dc8e66f1757
LuisFOsG <luisfosgdev@gmail.com>
2020-03-14 17:16:45 -0500

    feat(Root): get query to config

--- src/functions/settings.js
+++ src/functions/settings.js
@@ -1,10 +1,24 @@
 import { $, addEvent } from '@/util/domElements'
 
+import { getQuery } from '@/functions/query'
+
 const settings = $('#settings')
+export let isActiveSettings = false
+
+/* Inputs */
+const inputQuery = $('#query-bg')
 
 const changeVisibility = () => {
   settings.classList.toggle('visible')
   settings.classList.toggle('invisible')
+
+  isActiveSettings = !!settings.classList.contains('visible')
+  if (isActiveSettings) setValues()
+}
+
+const setValues = () => {
+  const queryBg = getQuery()
+  inputQuery.value = queryBg
 }
 
 addEvent($('#settings-btn'), 'click', changeVisibility)
