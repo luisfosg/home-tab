b36944937cdc9fc9f63fef88cf657404ad4f8b3b
LuisFOsG <luisfosgdev@gmail.com>
2020-04-27 15:13:30 -0500

    feat(Root): verify checked search engine

--- src/functions/search.js
+++ src/functions/search.js
@@ -38,10 +38,11 @@ export const loadSearchEngine = () => {
 
   $searchsEngine.innerHTML = ''
   for (const [name, url] of Object.entries(useSearchEngine)) {
+    const isChecked = defaultSearchEngine === name
     $searchsEngine.innerHTML += `
       <label class="ml-2 mt-2 cursor-pointer">
-        <input class="accent-slate-800" type="radio" value="${name}" name="radio">
-        <span>${capitalize(name)}</span>
+        <input class="accent-slate-800 cursor-pointer" type="radio" value="${name}" name="radio" ${isChecked && 'checked'}>
+        <span class="select-none">${capitalize(name)}</span>
         <input class="px-4 py-2 ml-3 text-lg rounded shadow-sm outline-none appearance-none bg-slate-600/50 md:text-xl h-8 hover:bg-gray-600/80" placeholder="${capitalize(name)} - Search Engine" value="${url}">
       </label>
     `
