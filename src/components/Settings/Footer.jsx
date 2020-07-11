eb6e3f561ab796276e7842cd4898a11844dd051b
LuisFOsG <luisfosgdev@gmail.com>
2020-07-10 19:09:18 -0500

    feat(Root): disabled buttons on save

--- src/components/Settings/Footer.jsx
+++ src/components/Settings/Footer.jsx
@@ -10,6 +10,8 @@ const FooterModal = () => {
     settings.setUpdateSettings(true)
   }
 
+  const isUpdated = settings.updateSettings
+
   return (
     <footer className='absolute bottom-0 left-0 flex items-center justify-between w-full h-16 px-2 select-none rounded-b-xl bg-slate-600/50 backdrop-blur-sm'>
       <div className='pl-2 text-base font-bold text-zinc-50/30 hover:text-zinc-50/50'>
@@ -17,9 +19,9 @@ const FooterModal = () => {
       </div>
       <div>
 
-        <button onClick={handleSave} className='flex items-center justify-center px-3 py-2 text-lg font-semibold rounded-lg bg-slate-600 hover:bg-slate-800 disabled:hover:bg-slate-600'>
+        <button onClick={handleSave} disabled={isUpdated} className='flex items-center justify-center px-3 py-2 text-lg font-semibold rounded-lg bg-slate-600 hover:bg-slate-800 disabled:bg-slate-400 disabled:hover:bg-slate-600'>
           {
-            settings.updateSettings ? <Loading /> : 'Guardar Cambios'
+            isUpdated ? <Loading /> : 'Guardar Cambios'
           }
         </button>
 
--- src/components/Settings/Header.jsx
+++ src/components/Settings/Header.jsx
@@ -5,11 +5,13 @@ import HomeTabContext from '@/context/hometabContext'
 import Close from '@/components/Icons/Close'
 
 const HeaderSettings = () => {
-  const { setOpenSettings } = useContext(HomeTabContext)
+  const { settings, setOpenSettings } = useContext(HomeTabContext)
 
   const handleClose = () => {
     setOpenSettings(false)
   }
+
+  const isUpdated = settings.updateSettings
 
   return (
     <header className='sticky top-0 left-0 flex items-center justify-between h-10 px-2 select-none rounded-t-xl bg-slate-500/20 backdrop-blur-sm'>
@@ -17,7 +19,7 @@ const HeaderSettings = () => {
       <div className='ml-2 text-xl font-bold text-white md:ml-0'>Home Tab <small className='text-sm text-slate-400'>v1.0.12</small></div>
       <div>
 
-        <button onClick={handleClose} className='flex items-center justify-center p-2 rounded-full group w-7 h-7 hover:bg-slate-800/80'>
+        <button disabled={isUpdated} onClick={handleClose} className='flex items-center justify-center p-2 rounded-full group w-7 h-7 hover:bg-slate-800/80 disabled:bg-slate-800'>
           <Close />
         </button>
 
