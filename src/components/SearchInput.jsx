f4ef1275204425882bc8057335f5275335dee08f
LuisFOsG <luisfosgdev@gmail.com>
2020-07-16 16:54:30 -0500

    feat(Root): select search engine

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,6 +1,12 @@
+import { getStorage } from '@/services/storage'
+
+import { defaultSearchEngine } from '@/config.json'
+
 const SearchInput = ({ value, setValue }) => {
+  const storageSearchEngine = getStorage('defaultSearchEngine') || defaultSearchEngine
+
   return (
-    <input value={value} onChange={e => setValue(e.target.value)} className='w-full h-full px-10 py-2 text-xl rounded shadow-lg appearance-none select-none bg-slate-900/50 md:text-2xl hover:bg-gray-800/80' autoFocus placeholder='Busca en Google o escribe una URL' type='text' />
+    <input value={value} onChange={e => setValue(e.target.value)} className='w-full h-full px-10 py-2 text-xl rounded shadow-lg appearance-none select-none bg-slate-900/50 md:text-2xl hover:bg-gray-800/80' autoFocus placeholder={`Busca en ${storageSearchEngine} o escribe una URL`} type='text' />
   )
 }
 
--- src/components/SearchsEngine.jsx
+++ src/components/SearchsEngine.jsx
@@ -1,31 +1,32 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 
-import { getStorage } from '@/services/storage'
+import HomeTabContext from '@/context/hometabContext'
+import { getStorage, setStorage } from '@/services/storage'
 import { capitalize } from '@/services/util'
 
 import Container from '@/components/Settings/ContainerSetting'
 import Title from '@/components/Settings/TitleSetting'
 import Input from '@/components/Settings/InputSetting'
 
-/* import Close from '@/components/Icons/Close' */
+import Close from '@/components/Icons/Close'
 
 import { searchEngine, defaultSearchEngine } from '@/config.json'
 
-const SearchEngine = ({ name, url, select }) => {
-  const isChecked = name === (select || defaultSearchEngine)
+const SearchEngine = ({ name, url, select, handleChange }) => {
+  const isChecked = name === select
 
   return (
     <div className='mt-5 grid grid-cols-3 gap-2 bg-slate-900/50 p-2 rounded-md'>
       <div className='col-span-2 flex items-center justify-start'>
-        <input id={name} className='accent-slate-400 caret-slate-800 h-4 w-4 cursor-pointer' type='radio' value={name} name='search-engine' defaultChecked={isChecked} />
+        <input id={name} className='accent-slate-400 caret-slate-800 h-4 w-4 cursor-pointer' type='radio' value={name} onChange={handleChange} name='search-engine' checked={isChecked} />
         <label htmlFor={name} className='ml-1 cursor-pointer select-none'>{capitalize(name)}</label>
       </div>
 
-      {/* <div className='flex items-center justify-end'>
+      <div className='flex items-center justify-end'>
         <button className='p-2 flex items-center justify-center w-7 h-7 rounded-lg bg-red-500/50 hover:bg-red-600'>
           <Close />
         </button>
-      </div> */}
+      </div>
 
       <div className='col-span-full flex items-center justify-center'>
         <Input type='short' value={url} placeholder={`${capitalize(name)} - Search Engine`} />
@@ -36,14 +37,39 @@ const SearchEngine = ({ name, url, select }) => {
 
 const SearchsEngine = () => {
   const [searchs, setSearchs] = useState([])
-  const storageSearchEngine = getStorage('defaultSearchEngine')
+  const storageSearchEngine = getStorage('defaultSearchEngine') || defaultSearchEngine
+  const [selected, setSelected] = useState(storageSearchEngine)
+
+  const { settings } = useContext(HomeTabContext)
 
   useEffect(() => {
+    settings.updateSetting({
+      name: 'searchEngine',
+      progress: true
+    })
+
     const savedSearchEngine = getStorage('searchsEngine')
     const useSearchEngine = savedSearchEngine || searchEngine
 
     setSearchs(Object.entries(useSearchEngine))
   }, [])
+
+  useEffect(() => {
+    if (settings.updateSettings) handleSave()
+  }, [settings.updateSettings])
+
+  const handleSave = () => {
+    setStorage('defaultSearchEngine', selected)
+    settings.updateSetting({
+      name: 'searchEngine',
+      success: true
+    })
+  }
+
+  const handleChange = (e) => {
+    const { value } = e.target
+    setSelected(value)
+  }
 
   return (
     <Container center>
@@ -52,7 +78,13 @@ const SearchsEngine = () => {
         {
           searchs.map((search, index) => {
             return (
-              <SearchEngine key={index} name={search[0]} url={search[1]} select={storageSearchEngine} />
+              <SearchEngine
+                key={index}
+                name={search[0]}
+                url={search[1]}
+                select={selected}
+                handleChange={handleChange}
+              />
             )
           })
         }
