9f2bfb23ab229eba438df09ec7ab9313c57ea388
LuisFOsG <luisfosgdev@gmail.com>
2020-08-06 18:09:12 -0500

    refactor(Root): use array in verify name storage

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,15 +1,9 @@
 import { searchEngine } from '@/config.json'
 
-const STORAGE_VALUES = {
-  QUERY: 'query',
-  WALLPAPER: 'wallpaper',
-  OWN_BG: 'ownBg',
-  PIN: 'pin',
-  TIME: 'time',
-
-  DEFAULT_SEARCH_ENGINE: 'defaultSearchEngine',
-  SEARCHS_ENGINE: 'searchsEngine'
-}
+const STORAGE_VALUES = [
+  'query', 'wallpaper', 'ownBg', 'pin',
+  'time', 'defaultSearchEngine', 'searchsEngine'
+]
 
 const sendError = () => {
   console.error('Storage name not found')
@@ -19,62 +13,40 @@ export const deleteAllStorage = () => {
   window.localStorage.clear()
 }
 
-export const setStorageSearch = (nameSelect, value) => {
-  const [name, select] = nameSelect.split('.')
-
-  for (const key in STORAGE_VALUES) {
-    if (STORAGE_VALUES[key] === name) {
-      const data = getStorage(name) || searchEngine
-      data[select] = value
-
-      setStorage(name, data)
-      return
-    }
-  }
-}
-
 export const setStorage = (name, value) => {
-  for (const key in STORAGE_VALUES) {
-    if (STORAGE_VALUES[key] === name) {
-      window.localStorage.setItem(name, JSON.stringify(value))
-      return
-    }
-  }
+  if (!STORAGE_VALUES.includes(name)) return sendError()
 
-  sendError()
+  window.localStorage.setItem(name, JSON.stringify(value))
 }
 
 export const getStorage = (name) => {
-  for (const key in STORAGE_VALUES) {
-    if (STORAGE_VALUES[key] === name) {
-      return JSON.parse(window.localStorage.getItem(name)) || null
-    }
-  }
+  if (!STORAGE_VALUES.includes(name)) return sendError()
 
-  sendError()
+  return JSON.parse(window.localStorage.getItem(name)) || null
+}
+
+export const setStorageSearch = (nameSelect, value) => {
+  const [name, select] = nameSelect.split('.')
+  if (!STORAGE_VALUES.includes(name)) return sendError()
+
+  const data = getStorage(name) || searchEngine
+  data[select] = value
+
+  setStorage(name, data)
 }
 
 export const deleteStorageSearch = (nameSelect) => {
   const [name, select] = nameSelect.split('.')
+  if (!STORAGE_VALUES.includes(name)) return sendError()
 
-  for (const key in STORAGE_VALUES) {
-    if (STORAGE_VALUES[key] === name) {
-      const data = getStorage(name) || searchEngine
-      delete data[select]
+  const data = getStorage(name) || searchEngine
+  delete data[select]
 
-      setStorage(name, data)
-      return
-    }
-  }
+  setStorage(name, data)
 }
 
 export const deleteStorage = (name) => {
-  for (const key in STORAGE_VALUES) {
-    if (STORAGE_VALUES[key] === name) {
-      window.localStorage.removeItem(name)
-      return
-    }
-  }
+  if (!STORAGE_VALUES.includes(name)) return sendError()
 
-  sendError()
+  window.localStorage.removeItem(name)
 }
